326bb4d5c46f3180fb5b432ca4dda24ccabbb354
brethower-k <brethower.k@gmail.com>
2020-06-19 21:00:42 -0400

    remove heavy backgrounds

--- examples/nike-air-bubble/gif_main.js
+++
@@ -1,7 +0,0 @@
-window.wallpaperPropertyListener = {
-    applyUserProperties: (properties) => {
-        if (properties.size) {
-            document.getElementById("main-img").style.height = properties.size.value + "px";
-        }
-    }
-};
